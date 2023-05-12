82a002690994c85ee8b893a11b3b71fec9d54ea0
rica213 <25f5k252l@mozmail.com>
2023-05-12 22:26:44 +0300

    Create custom methods

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,4 @@
 class Like < ApplicationRecord
-  belongs_to :user, counter_cache: true
-  belongs_to :post, counter_cache: true
+  belongs_to :author, class_name: 'User', foreign_key: :author_id
+  belongs_to :post
 end
-# Path: app/models/like.rb
