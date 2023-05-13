b2412e4c82bd5e4b2fdad56c01b637eae6d6b702
rica213 <25f5k252l@mozmail.com>
2023-05-13 13:49:42 +0300

    Use of Active Record Callbacks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Associations
+  # A user has many posts
+  # A user has many comments
+  # A user has many likes
   has_many :posts, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
@@ -16,8 +20,14 @@ class User < ApplicationRecord
     posts.order(created_at: :desc).limit(3)
   end
 
+  # Update the posts counter every time a new post
+  # is created for a given user
+  after_save :update_posts_count
+
   # Updates the posts counter for a user
   # @returns {Integer} the number of posts for a given user
+  private
+
   def update_posts_count
     update(posts_count: posts.count)
   end
