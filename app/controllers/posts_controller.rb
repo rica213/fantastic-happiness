c255505d3d1c9e09776a48d75b29149784cbe85a
rica213 <25f5k252l@mozmail.com>
2023-05-26 22:39:51 +0300

    Implement actions for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,15 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    # retrieve posts associated to @user and
+    # eager-loading the associated comments
+    @posts = @user.posts.includes(:comments)
+  end
 
-  def show; end
+  def show
+    @user = User.find(params[:user_id])
+    # retrieve the post associated to @user and
+    # eager-loading the associated comments
+    @post = @user.posts.includes(:comments).find(params[:id])
+  end
 end
