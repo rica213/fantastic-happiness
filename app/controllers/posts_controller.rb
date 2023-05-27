f4c767aa30df172dc9b15ceb6f2ac9d37bac3e93
rica213 <25f5k252l@mozmail.com>
2023-05-27 15:39:24 +0300

    Implement controller action new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+ # before_action :authenticate_user!, only: [:new]
   def index
     @user = User.find(params[:user_id])
     # retrieve posts associated to @user and
@@ -11,5 +12,9 @@ class PostsController < ApplicationController
     # retrieve the post associated to @user and
     # eager-loading the associated comments
     @post = @user.posts.includes(:comments).find(params[:id])
+  end
+
+  def new
+    @post = Post.new
   end
 end
