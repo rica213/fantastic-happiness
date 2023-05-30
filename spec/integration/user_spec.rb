e500211864069ba577b6b1b4be8647db96243c49
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-05-30 12:41:07 +0300

    Add user index tests for integration

--- spec/integration/user_spec.rb
+++ spec/integration/user_spec.rb
@@ -1,4 +1,42 @@
 require 'rails_helper'
+require 'capybara/rspec'
+require 'capybara/rails'
+
+describe 'testing users/index', type: :feature do
+  before(:each) do
+    @user1 = User.create(name: 'Mert', photo: 'https://somewhere.com/an_ordinary_photo.jpg', bio: 'Web Developer guy')
+
+    @user2 = User.create(name: 'Larissa', photo: 'https://somewhere.com/an_ordinary_photo.jpg', bio: 'Bee keeper and bee lover')
+
+    @user3 = User.create(name: 'Burak', photo: 'https://somewhere.com/an_ordinary_photo.jpg', bio: 'Software developer from Turkey')
+
+    @users = User.all
+    visit users_path
+  end
+
+  it 'diplays user name of all users' do
+    @users.each do |user|
+      expect(page).to have_content(user.name)
+    end
+  end
+
+  it 'displays profile picture for each user' do
+    @users.each do |_user|
+      expect(page).to have_css('img')
+    end
+  end
+
+  it 'displays the number of posts each user has written' do
+    @users.each do |user|
+      expect(page).to have_content "Number of posts: #{user.posts_count}"
+    end
+  end
+
+  it 'redirects to user\'s show page when clicked' do
+    click_on @user2.name
+    expect(page).to have_current_path user_path(@user2.id)
+  end
+end
 
 RSpec.describe 'User View', type: :feature do
   before :each do
