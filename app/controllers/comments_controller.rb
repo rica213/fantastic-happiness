a3ab73dc4d8359b6853d49e6986213b85defac7a
rica213 <25f5k252l@mozmail.com>
2023-06-02 16:20:43 +0300

    Add api endpoint to list all comments for a certain user in a certain post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,25 @@
 class CommentsController < ApplicationController
+
+  # user_post_comments
+  # GET /users/:user_id/posts/:post_id/comments
+  def index
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+    @comments = @post.comments.includes(:author)
+
+    # api endpoint to list all comments for a user's post
+    render json: @comments
+  end
+
+  # new_user_post_comment 
+  # GET /users/:user_id/posts/:post_id/comments/new
   def new
     @comment = Comment.new
     @post = Post.find(params[:post_id])
   end
 
+  # user_post_comments
+  # POST /users/:user_id/posts/:post_id/comments
   def create
     @comment = current_user.comments.create(comment_params)
     @comment.post_id = params[:post_id]
@@ -13,8 +29,10 @@ class CommentsController < ApplicationController
     else
       render :new, notice: 'Comment failed to be created.'
     end
+    
   end
 
+  # DELETE /users/:user_id/posts/:id
   def destroy
     @post = Post.find(params[:post_id])
     @comment = Comment.find(params[:id])
